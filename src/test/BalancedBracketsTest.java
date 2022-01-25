310a49580e85d573fd2933f5643d1f3b9c009195
Gia <gmay925@yahoo.com>
2022-01-24 21:47:36 -0500

    balancedStudio

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,6 +1,7 @@
 package test;
 
 import org.junit.Test;
+import org.junit.experimental.theories.suppliers.TestedOn;
 
 import static org.junit.Assert.*;
 
@@ -11,6 +12,51 @@ public class BalancedBracketsTest {
     public void emptyTest() {
         assertEquals(true, true);
     }
+    @Test
+    public void balancedBrackets(){
+        assertTrue(main.BalancedBrackets.hasBalancedBrackets("[Launch]"));
+    }
+    @Test
+    public void unBalancedBrackets() {
+        assertFalse(main.BalancedBrackets.hasBalancedBrackets("[go"));
+    }
+    @Test
+    public void nullString(){
+        assertTrue(main.BalancedBrackets.hasBalancedBrackets(" "));
+    }
+    @Test
+    public void string(){
+        assertTrue(main.BalancedBrackets.hasBalancedBrackets("String"));
+    }
+    @Test
+    public void doubleBrackets(){
+        assertTrue(main.BalancedBrackets.hasBalancedBrackets("[[double]]"));
+    }
+    @Test
+    public void midBrackets(){
+        assertFalse(main.BalancedBrackets.hasBalancedBrackets("lau[nch"));
+    }
+    @Test
+    public void nonStringChar(){
+        assertTrue(main.BalancedBrackets.hasBalancedBrackets("[this + that] "));
+    }
+    @Test
+    public void invertedBracket() {
+        assertTrue(main.BalancedBrackets.hasBalancedBrackets("] not ok["));
+    }
+    @Test
+    public void bracket(){
+        assertTrue(main.BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+//    @Test
+//    public void charOnly(){
+//        assertFalse(main.BalancedBrackets.hasBalancedBrackets("asl");
+//    }
+@Test
+    public void singleBrackets(){
+        assertTrue(main.BalancedBrackets.hasBalancedBrackets("[Being single]"));
+}
+
 
 
 }
